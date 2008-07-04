18bba1a2a733391c5ad9afe3cb2ac1cc9ab1bb8a
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-07-04 15:58:08 +0000

    merged with puzzle
    git-svn-id: https://svn/ipuppet/trunk/modules/postgres@1742 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,45 @@
-# modules/pgsql/manifests/init.pp - manage pgsql stuff
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
+#
+# postgres module
+#
+# Copyright 2008, Puzzle ITC
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
+# Module is base on the one from the immerda project
+# https://git.puppet.immerda.ch/module-pgsql
+# as well on Luke Kanies 
+# http://github.com/lak/puppet-postgres/tree/master
+#
+# modules_dir { \"postgres\": }
 
-# modules_dir { "pgsql": }
+class postgres {
+    case $operatingsystem {
+        gentoo: { include postgres::gentoo } 
+        centos: { include postgres::centos } 
+        default: { include postgres::base }
+    }
+}
 
-class pgsql::server {
+class postgres::base {
     package { 'postgresql':
         ensure => present,
-        category => $operatingsystem ? {
-            gentoo => 'dev-db',
-            default => '',
-        }
     }
 
     service{'postgresql':
         enable => true,
         ensure => running,
+        hasstatus => true,
         require => Package[postgresql],
+    }
+
+    file{'/etc/cron.d/pgsql_backup.cron':
+        source => "puppet://$server/postgres/backup/pgsql_backup.cron",
+        owner => root, group => 0, mode => 0600;
     }
 
     if $use_munin {
@@ -24,3 +47,16 @@ class pgsql::server {
     }
 }
 
+class postgres::gentoo inherits postgres::base {
+    Package['postgresql']{
+        category => 'dev-db',
+    }
+}
+
+class postgres::centos inherits postgres::base {
+	package { [ruby-postgres, postgresql-server]: ensure => installed }
+
+    Service[postgresql]{
+        require +> [ Package[postgresql-server] ],
+    }
+}
