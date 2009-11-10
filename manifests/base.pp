c82c789d04a802a7ebb7770d6f92b2d2ad9ed95e
Simon Josi <josi@puzzle.ch>
2009-11-10 16:16:11 +0100

    we have to do some serious debugging here...

--- manifests/base.pp
+++ manifests/base.pp
@@ -10,44 +10,44 @@ class postgres::base {
         ensure => running,
         hasstatus => true,
     }
-    file{'/var/lib/pgsql/data/pg_hba.conf':
-            source => [
-                "puppet://$server/site-postgres/${fqdn}/pg_hba.conf",
-                "puppet://$server/site-postgres/pg_hba.conf",
-                "puppet://$server/postgres/config/pg_hba.conf.${operatingsystem}",
-                "puppet://$server/postgres/config/pg_hba.conf"
-            ],
-            notify => Service[postgresql],
-            require => Package[postgresql-server],
-            owner => postgres, group => postgres, mode => 0600;
-    }
-    file{'/var/lib/pgsql/data/postgresql.conf':
-            source => [
-                "puppet://$server/site-postgres/${fqdn}/postgresql.conf",
-                "puppet://$server/site-postgres/postgresql.conf",
-                "puppet://$server/postgres/config/postgresql.conf.${operatingsystem}",
-                "puppet://$server/postgres/config/postgresql.conf"
-            ],
-            notify => Service[postgresql],
-            require => Package[postgresql-server],
-            owner => postgres, group => postgres, mode => 0600;
-    }
-    file{'/var/lib/pgsql/backups':
-        ensure => directory,
-        require => Package[postgresql-server],
-        owner => postgres, group => postgres, mode => 0700;
-    }
-    file{'/etc/cron.d/pgsql_backup.cron':
-        source => "puppet://$server/postgres/backup/pgsql_backup.cron",
-        require => File['/var/lib/pgsql/backups'],
-        owner => root, group => 0, mode => 0600;
-    }
-    file{'/etc/cron.d/pgsql_vacuum.cron':
-        source => "puppet://$server/postgres/maintenance/pgsql_vacuum.cron",
-        require => Package[postgresql-server],
-        owner => root, group => 0, mode => 0600;
-    }
-    if $use_munin {
-        include postgres::munin
-    }
+#    file{'/var/lib/pgsql/data/pg_hba.conf':
+#           source => [
+#                "puppet://$server/site-postgres/${fqdn}/pg_hba.conf",
+#                "puppet://$server/site-postgres/pg_hba.conf",
+#                "puppet://$server/postgres/config/pg_hba.conf.${operatingsystem}",
+#                "puppet://$server/postgres/config/pg_hba.conf"
+#            ],
+#            notify => Service[postgresql],
+#            require => Package[postgresql-server],
+#            owner => postgres, group => postgres, mode => 0600;
+#    }
+#    file{'/var/lib/pgsql/data/postgresql.conf':
+#            source => [
+#                "puppet://$server/site-postgres/${fqdn}/postgresql.conf",
+#                "puppet://$server/site-postgres/postgresql.conf",
+#                "puppet://$server/postgres/config/postgresql.conf.${operatingsystem}",
+#                "puppet://$server/postgres/config/postgresql.conf"
+#            ],
+#            notify => Service[postgresql],
+#            require => Package[postgresql-server],
+#            owner => postgres, group => postgres, mode => 0600;
+#    }
+#    file{'/var/lib/pgsql/backups':
+#        ensure => directory,
+#        require => Package[postgresql-server],
+#        owner => postgres, group => postgres, mode => 0700;
+#    }
+#    file{'/etc/cron.d/pgsql_backup.cron':
+#        source => "puppet://$server/postgres/backup/pgsql_backup.cron",
+#        require => File['/var/lib/pgsql/backups'],
+#        owner => root, group => 0, mode => 0600;
+#    }
+#    file{'/etc/cron.d/pgsql_vacuum.cron':
+#        source => "puppet://$server/postgres/maintenance/pgsql_vacuum.cron",
+#        require => Package[postgresql-server],
+#        owner => root, group => 0, mode => 0600;
+#    }
+#    if $use_munin {
+#        include postgres::munin
+#    }
 }
