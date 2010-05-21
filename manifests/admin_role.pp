e3434af719e41ae5d9988370e939a05fb19cc7a3
Simon Josi <josi@puzzle.ch>
2010-05-21 16:01:02 +0200

    add admnin_role define

---
+++ manifests/admin_role.pp
@@ -0,0 +1,10 @@
+define postgres::admin_role(
+    $ensure = present,
+    $password
+){
+  postgres::role{$name:
+    password => $password,
+    options => "SUPERUSER CREATEDB CREATEROLE",
+    ensure => $ensure;
+  }
+}
